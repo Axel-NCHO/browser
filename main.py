799d358c438b229af8f965198c7cf0ed54c4f5f3
Axel_N'CHO <naxelamour09+github@gmail.com>
2023-02-20 02:40:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets
+from browser import MainWindow
+import sys
+
+
+def start_browser():
+    webapp = QtWidgets.QApplication(sys.argv)
+    webapp.setApplicationName("Browser@alice")
+    if len(sys.argv) <=1:
+        _ = MainWindow()
+    else:
+        _ = MainWindow(sys.argv[1])
+    webapp.exec()
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    start_browser()
